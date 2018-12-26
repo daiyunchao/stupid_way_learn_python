b9756695c4b5667c932291104570bdd244efff3d
daiyunchao <312931671@qq.com>
2018-12-26 10:25:21 +0800

    完成了第12个小练习

---
+++ 笨办法学python/12.py
@@ -0,0 +1,9 @@
+age=raw_input("How old are you? ")
+height=raw_input("How tall are you? ")
+weight=raw_input("How much do you weight? ")
+
+print "So, you're %r old, %r tall and %r heavy." %(
+  age,height,weight)
+
+#使用raw_input输入参数,变成输入的提示信息
+#使用 python -m pydoc raw_input(window) 或 pydoc raw_input(非windows) 可查看文档
