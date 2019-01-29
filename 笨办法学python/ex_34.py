bc7e28fe2ec0e51f5df7bc5c81feda0bfccaaffa
daiyunchao <312931671@qq.com>
2019-01-29 10:38:09 +0800

    完成了章节34的;练习

---
+++ 笨办法学python/ex_34.py
@@ -0,0 +1,13 @@
+#coding=utf-8
+# 获取列表中的元素
+
+animals=['bear','python','peacock','kangaroo','whale','platypus']
+
+print "The animal at 1 %s" %animals[0]
+print "The 3rd animal %s" %animals[2]
+print "The 1st animal %s" %animals[0]
+print "The animal at 3 %s" %animals[2]
+print "The 5th animal %s " % animals[4]
+print "The animal at 2 %s" %animals[1]
+print "The 6th animal %s" %animals[5]
+print "The animal at 4 %s" %animals[3]
