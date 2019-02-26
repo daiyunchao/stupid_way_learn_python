320be54f066c9eea85b2252b8a91684a82f61ebe
daiyunchao <312931671@qq.com>
2019-02-26 13:17:55 +0800

    完成了章节49的练习

--- 笨办法学python/ex48/ex48/ex48.py
+++ 笨办法学python/ex48/ex48/ex48.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 class Lexicon(object):
   def __init__(self):
     self.directions=['north','south','east','west','down','up','left','right','back']
@@ -24,8 +25,72 @@ class Lexicon(object):
     
     return ret_vals
 
+class ParserError(Exception):
+  pass
 
+class Sentence(object):
+  def __init__(self,subject,verb,object):
+    self.subject=subject[1]
+    self.verb=verb[1]
+    self.object=object[1]
+  
+def peek(word_list):
+  if word_list:
+    word=word_list[0]
+    return word
+  else:
+    return None
+
+def match(word_list,expecting):
+  if word_list:
+    word=word_list.pop(0)
+
+    if word[0]==expecting:
+      return word
+    else:
+      return None
+
+def skip(word_list,word_type):
+  while peek(word_list)==word_type:
+    match(word_list,word_type)
+
+def parse_verb(word_list):
+  skip(word_list,'verb')
+
+  if peek(word_list)=='verb':
+    return match(word_list,'verb')
+  else:
+    raise ParserError("Expcted a verb next.")
+
+def parse_object(word_list):
+  skip(word_list,'stop')
+  next=peek(word_list)
+
+  if next=='noun':
+    return match(word_list,'noun')
+  if next=='direction':
+    return match(word_list,'direction')
+  else:
+    raise ParserError('Excepted a noun or direction next.')
+
+def parse_subject(word_list,subj):
+  verb=parse_verb(word_list)
+  obj=parse_object(word_list)
+  return Sentence(subj,verb,obj)
+
+def parse_sentence(word_list):
+  skip(word_list,'stop')
+  start=peek(word_list)
+
+  if start=='noun':
+    subj=match(word_list,'noun')
+    return parse_subject(word_list,subj)
+  elif start=='verb':
+    return parse_subject(word_list,('noun','player'))
+  else:
+    raise ParserError('Must start with subject,object,or verb not :%s' %start)  
 lexicon=Lexicon()
+
 
 # 判断元素是否在数组中 in
 #
--- 笨办法学python/ex48/tests/ex48_tests.py
+++ 笨办法学python/ex48/tests/ex48_tests.py
@@ -1,5 +1,5 @@
 from nose.tools import *
-from ex48.ex48 import lexicon
+from ex48.ex48 import lexicon,peek,match,skip,parse_verb,parse_object,parse_subject,parse_sentence,ParserError
 
 def test_directions():
     assert_equal(lexicon.scan("north"), [('direction', 'north')])
@@ -30,3 +30,48 @@ def test_errors():
     assert_equal(lexicon.scan("ASDFADFASDF"), [('error', 'ASDFADFASDF')])
     result = lexicon.scan("bear IAS princess")
     assert_equal(result, [('noun', 'bear'), ('error', 'IAS'), ('noun', 'princess')])
+
+def test_peek():
+  result=peek([('verb','kill'),('noun','door')])
+  assert_equal(result,('verb','kill'))
+
+def test_match():
+  word_listA=[('verb','kill'),('direction','north')]
+  result=match(word_listA,'verb')
+  assert_equal(result,('verb','kill'))
+
+def test_skip():
+     result=skip([('verb','kill'),('direction','north')],'verb')
+     assert_equal(result,('verb','kill'))
+     
+def test_parse_verb():
+    result=parse_verb([('verb','kill'),('direction','north')])
+    assert_equal(result,('verb','kill'))
+    assert_raises(ParserError,parse_verb,[('stop','the'),('direction','north')])
+    
+
+def test_parse_object():
+    result=parse_object([('stop','the'),('noun','door')])
+    assert_equal(result,('noun','door'))
+    result1=parse_object([('stop','the'),('direction','north')])
+    assert_equal(result1,('direction','north'))    
+    assert_raises(ParserError,parse_object,[('stop','the'),('verb','go')])
+
+def test_parse_subject():
+    result=parse_subject([('verb','go'),('stop','the'),('noun','door')],('m','k'))
+    assert_equal(result.subject,'k')
+    assert_equal(result.verb,'go')
+    assert_equal(result.object,'door')
+    
+def test_parse_sentence():
+    result=parse_sentence([('verb','go'),('stop','the'),('noun','door')])
+    assert_equal(result.subject,'player')
+    assert_equal(result.verb,'go')
+    assert_equal(result.object,'door')
+    
+    result1=parse_sentence([('stop','in'),('noun','door'),('verb','go'),('direction','north')])
+    assert_equal(result1.subject,'door')
+    assert_equal(result1.verb,'go')
+    assert_equal(result1.object,'north')
+    
+    assert_raises(ParserError,parse_sentence,[('stop','in'),('stop','the')])
