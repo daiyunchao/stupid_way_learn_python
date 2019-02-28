e19ce62972ee0b34b9333e8e115d9d73bdbde6dc
daiyunchao <312931671@qq.com>
2019-02-28 17:04:48 +0800

    完成了章节51的练习

--- 笨办法学python/web/gothonweb/bin/app.py
+++ 笨办法学python/web/gothonweb/bin/app.py
@@ -1,12 +1,22 @@
 import web
-urls = (
- '/', 'index'
+
+urls=(
+  '/hello','Index'
 )
-app = web.application(urls, globals())
-render = web.template.render('templates/')
-class index:
- def GET(self):
-  greeting = "Hello World"
-  return render.index(greeting = greeting)
-if __name__ == "__main__":
- app.run()
+
+
+app=web.application(urls,globals())
+
+render=web.template.render('templates/',base="layout")
+
+class Index(object):
+  def GET(self):
+    return render.hello_form()
+
+  def POST(self):
+    form=web.input(name="NoBody",greet="Hello")
+    greeting = "%s, %s " % (form.greet,form.name)
+    return render.index(greeting=greeting)
+
+if __name__=="__main__":
+  app.run()
